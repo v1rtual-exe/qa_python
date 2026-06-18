aa7a6f1706145d024ab8e11cd0db888ee5067d87
Artem Kinev <artem-kinev@yandex.ru>
2026-06-18 07:57:24 +0300

    Добавлены тесты для BooksCollector

--- tests.py
+++ tests.py
@@ -18,7 +18,69 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_success(self):
+        collector = BooksCollector()
+        collector.add_new_book('Война и мир')
+        assert 'Война и мир' in collector.get_books_genre()
+    def test_add_new_book_duplicate(self):
+        collector = BooksCollector()
+        collector.add_new_book('Война и мир')
+        collector.add_new_book('Война и мир')
+        assert len(collector.get_books_genre()) == 1
+    def test_add_new_book_max_length(self):
+        collector = BooksCollector()
+        long_name = 'А' * 41
+        collector.add_new_book(long_name)
+        assert len(collector.get_books_genre()) == 0
+    def test_add_new_book_empty_name(self):
+        collector = BooksCollector()
+        collector.add_new_book('')
+        assert len(collector.get_books_genre()) == 0
+    def test_get_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Война и мир')
+        collector.set_book_genre('Война и мир', 'Фантастика')
+        assert collector.get_book_genre('Война и мир') == 'Фантастика'
+
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book('Война и мир')
+        collector.add_new_book('Марсианин')
+        collector.set_book_genre('Война и мир', 'Фантастика')
+        collector.set_book_genre('Марсианин', 'Фантастика')
+        books = collector.get_books_with_specific_genre('Фантастика')
+        assert len(books) == 2
+        assert 'Война и мир' in books
+        assert 'Марсианин' in books
+
+    def test_get_books_for_children(self):
+        collector = BooksCollector()
+        collector.add_new_book('Война и мир')
+        collector.add_new_book('Оно')
+        collector.set_book_genre('Война и мир', 'Фантастика')
+        collector.set_book_genre('Оно', 'Ужасы')
+        children_books = collector.get_books_for_children()
+        assert 'Война и мир' in children_books
+        assert 'Оно' not in children_books
+
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Война и мир')
+        collector.add_book_in_favorites('Война и мир')
+        assert 'Война и мир' in collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book('Война и мир')
+        collector.add_book_in_favorites('Война и мир')
+        collector.delete_book_from_favorites('Война и мир')
+        assert 'Война и мир' not in collector.get_list_of_favorites_books()
+
+    def test_add_book_in_favorites_not_in_books(self):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Несуществующая книга')
+        assert len(collector.get_list_of_favorites_books()) == 0        

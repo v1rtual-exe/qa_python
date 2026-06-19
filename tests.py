2f24ada53a030acac004dade53b4ef32e6dcfbd1
Artem Kinev <artem-kinev@yandex.ru>
2026-06-19 07:44:36 +0300

    Исправлены замечания ревьюера: добавлена фикстура и отдельный тест для set_book_genre

--- tests.py
+++ tests.py
@@ -1,55 +1,47 @@
 import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+@pytest.fixture
+def collector():
+    return BooksCollector()
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
+    def test_add_new_book_add_two_books(self, collector):
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_genre()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
     @pytest.mark.parametrize('book_name', ['Война и мир', 'Марсианин', 'Гарри Поттер'])
-    def test_add_new_book_success(self, book_name):
-        collector = BooksCollector()
+    def test_add_new_book_success(self, collector, book_name):
         collector.add_new_book(book_name)
         assert book_name in collector.get_books_genre()
-    def test_add_new_book_duplicate(self):
-        collector = BooksCollector()
+
+    def test_add_new_book_duplicate(self, collector):
         collector.add_new_book('Война и мир')
         collector.add_new_book('Война и мир')
         assert len(collector.get_books_genre()) == 1
-    def test_add_new_book_max_length(self):
-        collector = BooksCollector()
+
+    def test_add_new_book_max_length(self, collector):
         long_name = 'А' * 41
         collector.add_new_book(long_name)
         assert len(collector.get_books_genre()) == 0
-    def test_add_new_book_empty_name(self):
-        collector = BooksCollector()
+
+    def test_add_new_book_empty_name(self, collector):
         collector.add_new_book('')
         assert len(collector.get_books_genre()) == 0
-    def test_get_book_genre(self):
-        collector = BooksCollector()
+
+    def test_set_book_genre(self, collector):
         collector.add_new_book('Война и мир')
         collector.set_book_genre('Война и мир', 'Фантастика')
         assert collector.get_book_genre('Война и мир') == 'Фантастика'
 
-    def test_get_books_with_specific_genre(self):
-        collector = BooksCollector()
+    def test_get_book_genre(self, collector):
+        collector.add_new_book('Война и мир')
+        collector.set_book_genre('Война и мир', 'Фантастика')
+        assert collector.get_book_genre('Война и мир') == 'Фантастика'
+
+    def test_get_books_with_specific_genre(self, collector):
         collector.add_new_book('Война и мир')
         collector.add_new_book('Марсианин')
         collector.set_book_genre('Война и мир', 'Фантастика')
@@ -59,8 +51,7 @@ class TestBooksCollector:
         assert 'Война и мир' in books
         assert 'Марсианин' in books
 
-    def test_get_books_for_children(self):
-        collector = BooksCollector()
+    def test_get_books_for_children(self, collector):
         collector.add_new_book('Война и мир')
         collector.add_new_book('Оно')
         collector.set_book_genre('Война и мир', 'Фантастика')
@@ -69,20 +60,17 @@ class TestBooksCollector:
         assert 'Война и мир' in children_books
         assert 'Оно' not in children_books
 
-    def test_add_book_in_favorites(self):
-        collector = BooksCollector()
+    def test_add_book_in_favorites(self, collector):
         collector.add_new_book('Война и мир')
         collector.add_book_in_favorites('Война и мир')
         assert 'Война и мир' in collector.get_list_of_favorites_books()
 
-    def test_delete_book_from_favorites(self):
-        collector = BooksCollector()
+    def test_delete_book_from_favorites(self, collector):
         collector.add_new_book('Война и мир')
         collector.add_book_in_favorites('Война и мир')
         collector.delete_book_from_favorites('Война и мир')
         assert 'Война и мир' not in collector.get_list_of_favorites_books()
 
-    def test_add_book_in_favorites_not_in_books(self):
-        collector = BooksCollector()
+    def test_add_book_in_favorites_not_in_books(self, collector):
         collector.add_book_in_favorites('Несуществующая книга')
-        assert len(collector.get_list_of_favorites_books()) == 0        
+        assert len(collector.get_list_of_favorites_books()) == 0
